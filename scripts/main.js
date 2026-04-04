0f25e322ded212ec82a3fdf0317df778ad744111
Dhrubajit-cmd <dhrubajitchakravorty@gmail.com>
2026-04-04 16:28:15 +0530

    Modified landing Page V5

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,10 @@ openBtn.onclick = () => {
 closeBtn.onclick = () => {
     modal.classList.add("hidden");
 };
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
