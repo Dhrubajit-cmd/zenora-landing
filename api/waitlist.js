eb07dd34fea86aecf8130f11968b15c8c8ccb40d
Dhrubajit-cmd <dhrubajitchakravorty@gmail.com>
2026-04-05 12:30:07 +0530

    Added google sheets api

--- api/waitlist.js
+++ api/waitlist.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
             await sheets.spreadsheets.values.append({
                 spreadsheetId: process.env.SHEET_ID,
-                range: "Zenora Waitlist!A:C",
+                range: "Sheet1!A:C",
                 valueInputOption: "RAW",
                 requestBody: {
                     values: [[name, email, new Date().toLocaleString()]],
